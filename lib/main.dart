875362edf8194f7f6cc14cc96c228f2f5911ce15
borispericat <borispericat@gmail.com>
2023-05-02 18:53:35 +0200

    ajout grahql

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+import 'package:graphql_flutter/graphql_flutter.dart';
 
-void main() {
+void main(List<String> arguments) async {
+  await initHiveForFlutter();
+
+  final HttpLink httpLink = HttpLink(
+    'https://graphql.anilist.co'
+    );
+
+  final Link link = httpLink;
+
+  ValueNotifier<GraphQLClient> client = ValueNotifier(
+    GraphQLClient(link: link, cache: GraphQLCache(store: HiveStore()))
+
+  )
+
   runApp(const MainApp());
 }
+
+
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
