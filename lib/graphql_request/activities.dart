80f98b27f9575314731ed1a8c801b587e9078952
borispericat <borispericat@gmail.com>
2023-05-08 14:57:47 +0200

    graphql social page request

--- lib/graphql_request/activities.dart
+++ lib/graphql_request/activities.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
-import 'dart:convert';
 
-void main() async {
-  await initHiveForFlutter();
-
-  runApp(const MainApp());
-}
 
  final HttpLink httpLink = HttpLink(
     'https://graphql.anilist.co'
@@ -23,8 +17,23 @@ void main() async {
   );
 
 String requestApi = """
-query (\$perPage: Int) { 
-  Page (perPage: \$perPage) {
+ query (\$id: Int) { 
+  Media (id: \$id, type: ANIME) { 
+    id
+    title {
+      romaji
+      english
+      native
+    }
+  }
+}
+""";
+
+//type request for disconnect home, to put as a string
+
+
+/*query ($perPage: Int) { 
+  Page (perPage: $perPage) {
      activities(isFollowing: true, sort: ID_DESC) {
       ... on TextActivity {
         id
@@ -158,66 +167,4 @@ query (\$perPage: Int) {
   
   }
 
-}
-""";
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return  GraphQLProvider(
-      client: client,
-      child: MaterialApp(
-      home: MyHomePage()
-    )
-    ) ;
-  }
-}
-
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  MyHomePageState createState() => MyHomePageState();
-   
-  
-}
-
-class MyHomePageState extends State<MyHomePage> {
-  
-  
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-          child: Query(
-            options: QueryOptions(
-              document: gql(requestApi),
-              variables: const {
-                'perPage': 50
-              },
-              pollInterval: const Duration(seconds: 10)
-          ), 
-            builder: (QueryResult result, { VoidCallback? refetch, FetchMore? fetchMore }) {
-              if (result.hasException) {
-                return Text(result.exception.toString());
-              }
-              if (result.isLoading) {
-                return const Text('Loading');
-              }
-
-              if (result == null) {
-                return const Text('No data');
-              }
-
-              var resultApi = json.encode(result.data);
-
-              return Text(resultApi);
-            }),
-        ),
-      );
-  }
-
-}
+}*/
