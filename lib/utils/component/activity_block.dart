a6069a8187f166da358bebfd9492f8a832cfb3d8
borispericat <borispericat@gmail.com>
2023-05-14 22:37:31 +0200

    covert image to left

--- lib/utils/component/activity_block.dart
+++ lib/utils/component/activity_block.dart
@@ -19,7 +19,7 @@ Widget build(BuildContext context) {
             options: QueryOptions(
               document: gql(requestApi),
               variables: const {
-                'perPage': 3
+                'perPage': 25
               },
               pollInterval: const Duration(minutes: 1)
           ), 
@@ -41,56 +41,70 @@ Widget build(BuildContext context) {
                   return Container(
                     color: const Color.fromRGBO(241,250,250,1),
                     margin: const EdgeInsets.symmetric(horizontal: 15),
+                    height: 125,
                     child: Row(
                       children: <Widget>[
-
-                          Container(
-                            height: 100,
-                            width: 70,
-                            decoration:BoxDecoration(
-                              image: DecorationImage(
-                              fit: BoxFit.contain,
-                              image: NetworkImage('${userName["media"]?["coverImage"]?["large"]}'),
+                        Flexible(
+                            flex: 2,
+                            child: Container(
+                              decoration:BoxDecoration(
+                                image: DecorationImage(
+                                fit: BoxFit.fill,
+                                image: NetworkImage('${userName["media"]?["coverImage"]?["large"]}'),
+                                
+                                )
+                              ) 
                               
+                                ),
+                          ),                       
+                        Expanded(
+                          flex: 6,
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 15),
+                            child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                            Container(
+                              width: 100,
+                              margin: const EdgeInsets.symmetric(vertical: 5),
+                              child: Row(
+                                children: <Widget>[
+                                  Expanded(
+                                    flex: 2,
+                                    
+                                    child: Text(
+                                      userName["user"]?['name'] ?? '',
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 1,
+                                      softWrap: false,
+                                    ),
+                                    ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: Image(
+                                      image: NetworkImage('${userName["user"]?["avatar"]?["large"]}'),
+                                      width: 40,
+                                      height: 40,
                               )
-                            ) 
-                            
+                                                    ),
+                                ],
                               ),
-                        
-                        Expanded(child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                          SizedBox(
-                            width: 100,
-                            child: Row(
-                              children: <Widget>[
-                                Expanded(
-                                  flex: 2,
-                                  child: Text(
-                                    userName["user"]?['name'] ?? '',
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 1,
-                                    softWrap: false,
-                                  ),
-                                  ),
-                                Expanded(
-                                  flex: 1,
-                                  child: Image(
-                                    image: NetworkImage('${userName["user"]?["avatar"]?["large"]}'),
-                                    width: 40,
-                                    height: 40,
-                            )
-                                                  ),
-                              ],
                             ),
-                          ),
-                        userName["text"] != null? Text(userName["text"])
-                        : Text('${userName["status"]} ${userName["progress"]} of ${userName["media"]?["title"]?["userPreferred"]}') 
-                        
-                        
-                          ]
-                        )),
+                                                  userName["text"] != null? Text(userName["text"])
+                                                  : Text(
+                            style: const TextStyle(
+                              fontSize: 12,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 3,
+                            softWrap: false,
+                            '${userName["status"]} ${userName["progress"]} of ${userName["media"]?["title"]?["userPreferred"]}') 
+                                                  
+                                                  
+                            ]
+                                                  ),
+                          )),
                   
                       ],
                     ),
