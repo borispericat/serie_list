211e03a1722a265a1fc06ea408bc6262795377e5
borispericat <borispericat@gmail.com>
2023-05-14 23:10:01 +0200

    border radius

--- lib/utils/component/activity_block.dart
+++ lib/utils/component/activity_block.dart
@@ -39,77 +39,80 @@ Widget build(BuildContext context) {
                   final userName = beta[index];
 
                   return Container(
-                    color: const Color.fromRGBO(241,250,250,1),
-                    margin: const EdgeInsets.symmetric(horizontal: 15),
+                    margin: const EdgeInsets.fromLTRB(15, 0,15, 24),
                     height: 125,
-                    child: Row(
-                      children: <Widget>[
-                        Flexible(
-                            flex: 2,
-                            child: Container(
-                              decoration:BoxDecoration(
-                                image: DecorationImage(
-                                fit: BoxFit.fill,
-                                image: NetworkImage('${userName["media"]?["coverImage"]?["large"]}'),
+                    color: const Color.fromRGBO(241,250,250,1),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Row(
+                        children: <Widget>[
+                          Flexible(
+                              flex: 2,
+                              child: Container(
+                                decoration:BoxDecoration(
+                                  image: DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image: NetworkImage('${userName["media"]?["coverImage"]?["large"]}'),
+                                  
+                                  )
+                                ) 
                                 
-                                )
-                              ) 
-                              
+                                  ),
+                            ),                       
+                          Expanded(
+                            flex: 6,
+                            child: Container(
+                              margin: const EdgeInsets.symmetric(horizontal: 15),
+                              child: SizedBox(
+                                height: double.infinity,
+                                child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                Container(
+                                  width: 100,
+                                  margin: const EdgeInsets.symmetric(vertical: 5),
+                                  child: Row(
+                                    children: <Widget>[
+                                      Expanded(
+                                        flex: 2,
+                                        
+                                        child: Text(
+                                          userName["user"]?['name'] ?? '',
+                                          overflow: TextOverflow.ellipsis,
+                                          maxLines: 1,
+                                          softWrap: false,
+                                        ),
+                                        ),
+                                      Expanded(
+                                        flex: 1,
+                                        child: Image(
+                                          image: NetworkImage('${userName["user"]?["avatar"]?["large"]}'),
+                                          width: 40,
+                                          height: 40,
+                                  )
+                                                        ),
+                                    ],
+                                  ),
                                 ),
-                          ),                       
-                        Expanded(
-                          flex: 6,
-                          child: Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 15),
-                            child: SizedBox(
-                              height: double.infinity,
-                              child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                              Container(
-                                width: 100,
-                                margin: const EdgeInsets.symmetric(vertical: 5),
-                                child: Row(
-                                  children: <Widget>[
-                                    Expanded(
-                                      flex: 2,
-                                      
-                                      child: Text(
-                                        userName["user"]?['name'] ?? '',
-                                        overflow: TextOverflow.ellipsis,
-                                        maxLines: 1,
-                                        softWrap: false,
-                                      ),
-                                      ),
-                                    Expanded(
-                                      flex: 1,
-                                      child: Image(
-                                        image: NetworkImage('${userName["user"]?["avatar"]?["large"]}'),
-                                        width: 40,
-                                        height: 40,
-                                )
+                                                      userName["text"] != null? Text(userName["text"])
+                                                      : Text(
+                                style: const TextStyle(
+                                  fontSize: 12,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 3,
+                                softWrap: false,
+                                '${userName["status"]} ${userName["progress"]} of ${userName["media"]?["title"]?["userPreferred"]}') 
+                                                      
+                                                      
+                                ]
                                                       ),
-                                  ],
-                                ),
                               ),
-                                                    userName["text"] != null? Text(userName["text"])
-                                                    : Text(
-                              style: const TextStyle(
-                                fontSize: 12,
-                              ),
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 3,
-                              softWrap: false,
-                              '${userName["status"]} ${userName["progress"]} of ${userName["media"]?["title"]?["userPreferred"]}') 
-                                                    
-                                                    
-                              ]
-                                                    ),
-                            ),
-                          )),
-                  
-                      ],
+                            )),
+                                      
+                        ],
+                      ),
                     ),
                   );
                 }
